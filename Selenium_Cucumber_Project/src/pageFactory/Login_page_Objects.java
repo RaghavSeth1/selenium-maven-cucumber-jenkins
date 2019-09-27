0d6658b1e2d61d16c51c6917cf24e0d737d8a119
raghav.seth <raghav.seth@ND10ANLPC157.INFO.CORP>
2019-09-27 11:11:23 +0530

    Login Update included error screenshot

--- Selenium_Cucumber_Project/src/pageFactory/Login_page_Objects.java
+++ Selenium_Cucumber_Project/src/pageFactory/Login_page_Objects.java
@@ -1,26 +1,40 @@
 package pageFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement; 
 import org.openqa.selenium.support.FindBy; 
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 
 public class Login_page_Objects {
-	//public static WebDriver driver=null;
-	final WebDriver driver;
-	@FindBy(how = How.ID , using = "txtUsername")
-	public WebElement txtbx_UserName;
-	
-	@FindBy(how = How.ID, using = "txtPassword")	 
-	 public WebElement txtbx_Password;
-	 
-	 @FindBy(how = How.NAME, using = "btnLogin")	 
-	 public WebElement btn_Login ;
-	 
-	
+	WebDriver driver;
 	public Login_page_Objects(WebDriver driver){ 		 
-	    this.driver = driver; 
-	 
-	    } 
+		this.driver = driver; 
+		PageFactory.initElements(driver, this);
+	}	
+	
+	
+	//Username field 
+	@FindBy(how = How.ID , using = "txtUsername")
+	public static WebElement txtbx_UserName;
+	
+	//Password field
+	@FindBy(how = How.ID, using = "txtPassword")	 
+	public static WebElement txtbx_Password;
 
+	//Login Button
+	@FindBy(how = How.ID, using = "btnLogin")	 
+	public static WebElement btn_Login ;
+	
+
+	public  void setUsername(String userName) {
+		txtbx_UserName.sendKeys(userName);		
+	}
+	public  void setPassword(String password) {
+		txtbx_Password.sendKeys(password);		
+	} 
+	public  void loginClick() {
+		btn_Login.click();
+	} 
 }
